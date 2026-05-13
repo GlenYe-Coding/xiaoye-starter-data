1547006891c56cf6165a2e7a6b7f3c75f36011c6
GlenYe-coding <2540463130@qq.com>
2026-05-13 16:34:43 +0800

    优化

--- src/main/java/com/xiaoye/starter/data/autoconfigure/DataAutoConfiguration.java
+++ src/main/java/com/xiaoye/starter/data/autoconfigure/DataAutoConfiguration.java
@@ -1,25 +1,30 @@
-package com.xiaoye.starter.data.configurer;
+package com.xiaoye.starter.data.autoconfigure;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
- * MyBatis Plus 自动配置
+ * Data 模块自动配置
  */
-@Configuration
-public class MyBatisPlusConfig {
+@AutoConfiguration
+public class DataAutoConfiguration {
 
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        // 分页插件
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
-        // 乐观锁插件
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return interceptor;
+    }
+
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new LogicSqlInjector();
     }
 }
